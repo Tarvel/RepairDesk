982158bae75b79a532717dcb867c8f63b41dcbc3
tarvel <taiyebabatundejames@gmail.com>
2026-06-22 18:53:33 +0100

    Switch to Vercel's zero-configuration engine for Django

--- build_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# build_files.sh
-
-# Install dependencies
-python3 -m pip install -r requirements.txt
-
-# Run collectstatic to gather static files
-python3 manage.py collectstatic --noinput --clear
